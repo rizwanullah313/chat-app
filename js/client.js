a287f062a28c915a872c9df93b7830004685d534
rizwanullah313 <ru323091@gmail.com>
2021-10-05 13:28:45 +0500

    Connect with App and Forms

---
+++ js/client.js
@@ -0,0 +1,5 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document/getElementById('messageInp');
+const messageContainer = document.getElementById(".container")
