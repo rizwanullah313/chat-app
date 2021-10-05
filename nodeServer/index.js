d6e18c91cd488f5389fb3deb481125c06d89d235
rizwanullah313 <ru323091@gmail.com>
2021-10-05 13:17:14 +0500

    Create NodeServer and connect to socket.io

---
+++ nodeServer/index.js
@@ -0,0 +1,19 @@
+// Node Server which will hanlde socket io connection
+
+// sudo npm install nodemon -g
+// npm install socket.io
+// npm init
+const io = require('socket.io')(8000)
+
+
+const users = {};
+
+io.on('connection', socket=>{ // socket.io instance hay jo k buhhut sarey io connection ko listen kaega
+    socket.on('new-user-joined', name=>{ // jo be aik particular connection k saath hoga hona chahye wo socket.io handle karega
+      users[socket.id]=name;   // jobe new join karega ye usko append kerdega ocket.id m
+      socket.broadcast.emit('user-joined', name); //jis ne jin kia usko chor k baqi sab k emit kerdega k is ne join kia hay chat
+    })
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+    });
+})
