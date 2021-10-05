ed8b9028fa36e75338db91d703cb241790e86b11
rizwanullah313 <ru323091@gmail.com>
2021-10-05 14:25:28 +0500

    solve huge error problem aloow aceess usinf cors

--- js/client.js
+++ js/client.js
@@ -1,5 +1,10 @@
 const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
-const messageInput = document/getElementById('messageInp');
+const messageInput = document.getElementById('messageInp')
 const messageContainer = document.getElementById(".container")
+
+
+
+const name = prompt("Enter Your Name to join Chat");
+socket.emit('new-user-joined', name)
